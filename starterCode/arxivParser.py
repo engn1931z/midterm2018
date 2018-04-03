4a23a0be119aa9a257a7d36d01c3ee7e70ecc61d
Rashid Zia <Rashid_Zia@brown.edu>
2018-04-03 12:08:59 -0400

    Uploading Exam

---
+++ starterCode/arxivParser.py
@@ -0,0 +1,21 @@
+import feedparser
+# import re
+# from bs4 import BeautifulSoup
+
+def parseRss(url='https://arxiv.org/rss/physics.optics'):
+  """ RSS Feed URL -> list of dictionaries for first five articles
+  
+  This function returns a list of dictionaries with the keys 'abstract', 'authors', 'title', and 'url'
+  for the first five articles on the specified arXiv RSS Feed url.
+  
+  The values for each of the five keys must be strings.
+  
+  The string for the authors key should parse the list of authors received from the feed into one single string.
+  
+  The default argument url='https://arxiv.org/rss/physics.optics' shows an example of the expected input.
+  """
+  
+  #YOUR CODE HERE: programmatically producing the articleList of dictionaries described above
+    
+  articlesList = [{}]
+  return articlesList
---
+++ starterCode/textToAudioVisual.py
@@ -0,0 +1,17 @@
+import os
+from gtts import gTTS
+from wordcloud import WordCloud
+
+exampleArticlesList=[{'abstract':'Cracked Pots Lecture','authors':'J. Carberry','articleID':'1801.99999','title':'Pyschoceramics','url':'https://arxiv.org/abs/1801.99999'}]
+exampleDirectory='public_files'
+def createAudioFiles(articlesList=exampleArticlesList, directoryPath=exampleDirectory):
+  """  list of article dictionaries -> 
+  For each article, create an MP3 audio file that includes title, authors, and abstract.
+  MP3 files should be stored in the specified directoryPath.
+  """
+
+def createWordCloudImages(articlesList=exampleArticlesList, directoryPath=exampleDirectory):
+  """ list of article dictionaries -> 
+  For each article, create a square PNG image file using the words from the abstract.
+  PNG files should be stored in the specified directoryPath.
+  """
